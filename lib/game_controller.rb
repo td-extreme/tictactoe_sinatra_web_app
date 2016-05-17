32adcda2295668f44995590b5c231b592389bd21
Tyler Decker <tyler.decker@gmail.com>
2016-05-17 14:50:34 -0500

    Redid tests and code for Controller. +
    Changed @img to @images in baord.erb

--- lib/game_controller.rb
+++ lib/game_controller.rb
@@ -4,28 +4,31 @@ $:.unshift(lib)
 require 'sinatra'
 require 'game'
 
-set :root, File.join(File.dirname(__FILE__), '..')
-set :views, Proc.new { File.join(root, "views") }
+class GameController < Sinatra::Base
 
-get '/' do
+  set :root, File.join(File.dirname(__FILE__), '..')
+  set :views, Proc.new { File.join(root, "views") }
+
   game = Game.new
-  @player_order = Game.player_order
-  @player_choices = Game.player_choices
-  erb :index
-end
 
-post '/newgame' do
-  @@game = Game.new
-  @@game.setup_game(params['Order'], params['Player2'])
-  @@game.play_ai_turn
-  @img = @@game.board_images
-  erb :play_move
-end
+  get '/' do
+    @player_choices = Game.player_choices
+    @player_order = Game.player_order
+    erb :index
+  end
 
-post '/playmove' do
-  @message = "That is not a valid move!" if !@@game.play_move(params['move'].to_i - 1)
-  @@game.play_ai_turn
-  @img = @@game.board_images
-  @results = @@game.results
-  @@game.game_over? ? (erb :play_again) : (erb :play_move)
+  post '/newgame' do
+    game.setup_game(params['Order'], params['Player2'])
+    game.play_ai_turn
+    @images = game.board_images
+    erb :play_move
+  end
+
+  post '/playmove' do
+    @message = "That is not a valid move!" if !game.play_move(params['move'].to_i - 1)
+    game.play_ai_turn
+    @results = game.results
+    @images = game.board_images
+    game.game_over? ? (erb :play_again) : (erb :play_move)
+  end
 end
